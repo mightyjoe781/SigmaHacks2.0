2d1970a9a2ceb5f1e2829f98203c69caaf3ed219
Barthwal <barthwalumang@gmail.com>
2020-07-25 11:43:25 +0530

    update news.js

---
+++ public/js/news.js
@@ -0,0 +1,12 @@
+// API for local news
+// initial location : Spain
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+ 
+fetch("http://api.coronatracker.com/news/trending?limit=10&offset&country=Spain&countryCode", requestOptions)
+    .then(response => response.text())
+    .then(result => document.getElementById("news").innerHTML = result)
+    .catch(error => console.log('error', error));
