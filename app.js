6a67a5b36fdbac6360ebb81c6ab177bb5762ae25
Sudhanshu Mohan <sudhanshumohan781@gmail.com>
2020-07-25 12:35:27 +0530

    Added empty pages with routes
    Done basic linking to the pages you guys can add starting your content now

--- app.js
+++ app.js
@@ -19,6 +19,35 @@ app.get("/",function(req,res){
     res.render("index");
 });
 
+//AMENITIES
+app.get("/amenities",function(req,res){
+	res.render("amenities");
+});
+
+//NEWS
+app.get("/news",function(req,res){
+	res.render("news1");
+});
+
+//FORMS ROUTE
+app.get("/form_donor",function(req,res){
+	res.render("form_donor");
+});
+
+app.get("/form_patient",function(req,res){
+	res.render("form_patient");
+});
+
+//COMMON POOL
+app.get("/common_pool",function(req,res){
+	res.render("common_pool");
+});
+
+//PLASMA BANK
+app.get("/plasma_bank",function(req,res){
+	res.render("plasma_bank");
+});
+
 //=================lISTENER PROCESS============//
 
 var port = process.env.PORT || 31000
